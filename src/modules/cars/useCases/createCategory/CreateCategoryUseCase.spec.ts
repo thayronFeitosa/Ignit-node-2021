948c97c9ebecf0611a00ea49ee0c366ac499e27d
thayron <thayronf3@gmail.com>
2021-09-20 06:08:39 -0300

    Teste de criaçao de categoria

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,15 +1,51 @@
-describe('Criar categoria', () => {
-  it('Espero que 2 + 2 seja 4', () => {
-    const soma = 2 + 2;
-    const result = 4;
+import { AppError } from "../../../../shared/errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
 
-    expect(soma).toBe(result);
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemoey: CategoriesRepositoryInMemory;
+
+describe('Create Category', () => {
+  beforeAll(() => {
+    categoriesRepositoryInMemoey = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemoey
+    );
+  })
+
+  it('should be able to create a new category', async () => {
+    const categor = {
+      name: 'Category Test',
+      description: 'Category description Test'
+    }
+    await createCategoryUseCase.execute({
+      name: categor.name,
+      description: categor.description
+    });
+
+    const categoryCreated = await categoriesRepositoryInMemoey.findByName(categor.name);
+
+    expect(categoryCreated).toHaveProperty('id');
   });
 
-  it('Espero que 2 + 2 não seja 5', () => {
-    const soma = 2 + 2;
-    const result = 5;
-    expect(soma).not.toBe(result);
+  it('should not be able to create a new category with same name exists', async () => {
+
+    expect(async () => {
+      const categor = {
+        name: 'Category Test',
+        description: 'Category description Test'
+      }
+      await createCategoryUseCase.execute({
+        name: categor.name,
+        description: categor.description
+      });
+
+      await createCategoryUseCase.execute({
+        name: categor.name,
+        description: categor.description
+      });
+    }).rejects.toBeInstanceOf(AppError)
+
 
   })
 })
